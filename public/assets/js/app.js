8dce42c8f296c15348a8d905b83d6a7ef78f718d
rherd <rob.a.herd@gmail.com>
2018-09-17 23:51:36 -0400

    initial

---
+++ public/assets/js/app.js
@@ -0,0 +1,75 @@
+//ajax call for scrape button
+$("#scrape").on("click", function() {
+    $.ajax({
+        method: "GET",
+        url: "/scrape",
+    }).done(function(data) {
+        console.log(data)
+        window.location = "/"
+    })
+});
+
+//remove navbar class
+// $(".navbar-nav li").click(function() {
+//    $(".navbar-nav li").removeClass("active");
+//    $(this).addClass("active");
+// });
+
+//ajax call for save button
+$(".save").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/save/" + thisId
+    }).done(function(data) {
+        window.location = "/"
+    })
+});
+
+//ajax call for deleting saved article
+$(".delete").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/delete/" + thisId
+    }).done(function(data) {
+        window.location = "/saved"
+    })
+});
+
+//ajax call for saving a new note
+$(".saveNote").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    if (!$("#noteText" + thisId).val()) {
+        alert("please enter a note to save")
+    }else {
+      $.ajax({
+            method: "POST",
+            url: "/notes/save/" + thisId,
+            data: {
+              text: $("#noteText" + thisId).val()
+            }
+          }).done(function(data) {
+              // Log the response
+              console.log(data);
+              // Empty the notes section
+              $("#noteText" + thisId).val("");
+              $(".modalNote").modal("hide");
+              window.location = "/saved"
+          });
+    }
+});
+
+//ajax call for deleting a note
+$(".deleteNote").on("click", function() {
+    var noteId = $(this).attr("data-note-id");
+    var articleId = $(this).attr("data-article-id");
+    $.ajax({
+        method: "DELETE",
+        url: "/notes/delete/" + noteId + "/" + articleId
+    }).done(function(data) {
+        console.log(data)
+        $(".modalNote").modal("hide");
+        window.location = "/saved"
+    })
+});
